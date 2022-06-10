69d42d50d6ffe95720b33c1cfcfea9c70dfce282
Josh Thompson <thompsonjoshd@gmail.com>
2022-06-10 03:51:01 -0600

    home_controller#index, strava_athelete_activity_getter

---
+++ app/models/strava_athlete_activity_getter.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+
+class StravaAthleteActivityGetter
+  def self.get_activity(athlete, strava_activity_id)
+    token = "70bf1be1d6e6ae36c1c64f0f5feae95d76c5ae43"
+    
+  end
+end
